545feb6cd190f51d41b7720bb8567183a915be9c
BF-boop <franz.bauhuber2@gmx.de>
2020-05-08 16:54:51 +0200

    Aufgabe  Duko

--- src/Aufgabe_Schleife_Arrays/Aufgabe_05/Zeitumrechnung.java
+++ src/Aufgabe_Schleife_Arrays/Aufgabe_05/Zeitumrechnung.java
@@ -5,23 +5,34 @@ import java.util.Scanner;
 
 /**
  * Dies ist die Dokumentation der Klasse Zeittumrechnung. Zeitumrechnung frägt den Benutzer
- * nach einer Sekundenzahl, die er in Stunde, Minuten und Sekunden umrecchnet
+ * nach einer Sekundenzahl, die er in Stunde, Minuten und Sekunden umrechnet
  *
  * @author Franz Bauhuber
  * @version 1.0
  */
 public class Zeitumrechnung {
     public static void main(String[] args) {
+        /**
+         * Attribute
+         */
         int eingabe, sekunden, minuten, stunden;
         Scanner scan = new Scanner(System.in);
-
+        /**
+         * Eingabe
+         */
         System.out.println("Bitte Sekundenzahl eingeben");
         eingabe = scan.nextInt();
+        /**
+         * Verarbeitung
+         */
         stunden = eingabe / 3600;
         sekunden = eingabe % 3600;
         minuten = sekunden / 60;
         sekunden = sekunden % 60;
+        /**
+         * Ausgabe
+         */
         System.out.println(eingabe + " Sekunden sind: ");
-        System.out.println(stunden +" Stunden, \n" + minuten + " Minuten, \n" + sekunden + " Sekunden");
+        System.out.println(stunden + " Stunden, \n" + minuten + " Minuten, \n" + sekunden + " Sekunden");
     }
 }
